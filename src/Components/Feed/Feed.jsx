28f235b967d09238f0209e0b08d1eb51f37824c2
zohaib <zohaibansari339@gmail.com>
2026-05-11 13:51:02 +0500

    feat: implement dynamic video fetching in Feed component and remove hardcoded thumbnails

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,114 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Feed.css'
-import thumbnail1 from '../../assets/thumbnail1.png'
-import thumbnail2 from '../../assets/thumbnail2.png'
-import thumbnail3 from '../../assets/thumbnail3.png'
-import thumbnail4 from '../../assets/thumbnail4.png'
-import thumbnail5 from '../../assets/thumbnail5.png'
-import thumbnail6 from '../../assets/thumbnail6.png'
-import thumbnail7 from '../../assets/thumbnail7.png'
-import thumbnail8 from '../../assets/thumbnail8.png'
+import { API_KEY, value_converter } from '../../data'
 
-const Feed = () => {
+const Feed = ({category}) => {
+
+  const [data, setData] = useState([])
+
+  const fetchData = async () => {
+    const videoListUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`
+    const response = await fetch(videoListUrl)
+    const result = await response.json()
+    setData(result.items || [])
+  }
+
+  useEffect(()=>{
+    fetchData();
+  },[category])
+
   return (
     <div className="feed">
-      <Link to={`video/20/4521`} className='card'>
-      <img src={thumbnail1} alt="" />
-      <h2>Best channal to learn coding that help you to be a web developer</h2>
-      <h3>Ansaritech</h3>
-      <p>15k views &bull; 2 days ago</p>
+      {data.map((item)=>{
+        return(
+        <Link to={`/video/${item.snippet.categoryId }/${item.id}`} className='card' key={item.id}>
+      <img src={item.snippet.thumbnails.medium.url} alt={item.snippet?.title || 'video thumbnail'} />
+      <h2>{item.snippet.title}</h2>
+      <h3>{item.snippet.channelTitle}</h3>
+      <p>{value_converter(item.statistics.viewCount)} views &bull; {item.snippet.publishedAt}</p>
     </Link>
-     <div className='card'>
-      <img src={thumbnail2} alt="" />
-      <h2>Best channal to learn coding that help you to be a web developer</h2>
-      <h3>Ansaritech</h3>
-      <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-      <img src={thumbnail3} alt="" />
-      <h2>Best channal to learn coding that help you to be a web developer</h2>
-      <h3>Ansaritech</h3>
-      <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-      <img src={thumbnail4} alt="" />
-      <h2>Best channal to learn coding that help you to be a web developer</h2>
-      <h3>Ansaritech</h3>
-      <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-      <img src={thumbnail5} alt="" />
-      <h2>Best channal to learn coding that help you to be a web developer</h2>
-      <h3>Ansaritech</h3>
-      <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-      <img src={thumbnail6} alt="" />
-      <h2>Best channal to learn coding that help you to be a web developer</h2>
-      <h3>Ansaritech</h3>
-      <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-      <img src={thumbnail7} alt="" />
-      <h2>Best channal to learn coding that help you to be a web developer</h2>
-      <h3>Ansaritech</h3>
-      <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-      <img src={thumbnail8} alt="" />
-      <h2>Best channal to learn coding that help you to be a web developer</h2>
-      <h3>Ansaritech</h3>
-      <p>15k views &bull; 2 days ago</p>
-    </div>
-    <div className='card'>
-      <img src={thumbnail1} alt="" />
-      <h2>Best channal to learn coding that help you to be a web developer</h2>
-      <h3>Ansaritech</h3>
-      <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-      <img src={thumbnail2} alt="" />
-      <h2>Best channal to learn coding that help you to be a web developer</h2>
-      <h3>Ansaritech</h3>
-      <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-      <img src={thumbnail3} alt="" />
-      <h2>Best channal to learn coding that help you to be a web developer</h2>
-      <h3>Ansaritech</h3>
-      <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-      <img src={thumbnail4} alt="" />
-      <h2>Best channal to learn coding that help you to be a web developer</h2>
-      <h3>Ansaritech</h3>
-      <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-      <img src={thumbnail5} alt="" />
-      <h2>Best channal to learn coding that help you to be a web developer</h2>
-      <h3>Ansaritech</h3>
-      <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-      <img src={thumbnail6} alt="" />
-      <h2>Best channal to learn coding that help you to be a web developer</h2>
-      <h3>Ansaritech</h3>
-      <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-      <img src={thumbnail7} alt="" />
-      <h2>Best channal to learn coding that help you to be a web developer</h2>
-      <h3>Ansaritech</h3>
-      <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-      <img src={thumbnail8} alt="" />
-      <h2>Best channal to learn coding that help you to be a web developer</h2>
-      <h3>Ansaritech</h3>
-      <p>15k views &bull; 2 days ago</p>
-    </div>
+        )
+      })}
+      
+     
     </div>
   )
 }
