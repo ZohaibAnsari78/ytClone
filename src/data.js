5478a1b9da2dfe8c48222f6d2d61ddf3f7bde6b7
zohaib <zohaibansari339@gmail.com>
2026-05-11 13:50:44 +0500

    feat: add value_converter function for formatting numerical values

--- src/data.js
+++ src/data.js
@@ -1,1 +1,15 @@
 export const API_KEY = 'AIzaSyDE6fRY9mDSgMOYYpOH4VMb8A8xynU73n0'
+
+export const value_converter =(value)=>{
+    if(value > 1000000)
+    {
+        return Math.floor(value/1000000) + 'M';
+    }
+    else if(value > 1000)
+    {
+        return Math.floor(value/1000) + 'K';
+    }
+    else{
+        return value;
+    }
+} 
