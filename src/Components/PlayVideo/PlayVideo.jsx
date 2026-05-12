b70bb778a67f854291ae8f727c4cf04747db96ab
zohaib <zohaibansari339@gmail.com>
2026-05-13 00:55:05 +0500

    feat: fetch and display comments for video in PlayVideo component

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -15,6 +15,7 @@ const PlayVideo = ({videoId}) => {
 
   const [apiData, setApiData] =useState(null);
   const [channalData, setChannalData] =useState(null);
+  const [commentData , setCommentData] = useState([]);
 
 
   const fetchVideoData = async ()=>{
@@ -26,17 +27,26 @@ const PlayVideo = ({videoId}) => {
   }
 
   
-    const fetchOtherData = async ()=>{
-      if (!apiData?.snippet?.channelId) return
-      // Fetch Channel Data
-      const channelDataUrl = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=${apiData.snippet.channelId}&key=${API_KEY}`
+  const fetchOtherData = async () => {
+    if (!apiData?.snippet?.channelId) return
+    // Fetch Channel Data
+    const channelDataUrl = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=${apiData.snippet.channelId}&key=${API_KEY}`
 
-      const response = await fetch(channelDataUrl)
-      const data = await response.json()
-      setChannalData(data.items?.[0] || null)
+    const response = await fetch(channelDataUrl)
+    const data = await response.json()
+    setChannalData(data.items?.[0] || null)
 
-
+    // fetch Comment Data
+    try {
+      const comment_url = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=50&videoId=${videoId}&key=${API_KEY}`
+      const commentRes = await fetch(comment_url)
+      const commentJson = await commentRes.json()
+      setCommentData(commentJson.items || [])
+    } catch (err) {
+      console.error('Failed to fetch comments', err)
+      setCommentData([])
     }
+  }
 
 
   useEffect(()=>{
@@ -54,7 +64,13 @@ const PlayVideo = ({videoId}) => {
   return (
     <div className='play-video'>
       {/* <video src={Video1} controls autoPlay muted></video>   */}
-      <iframe  src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
       <h3>{apiData?.snippet?.title ?? "Title Here"}</h3>
       <div className='video-play-info'>
         <p>{apiData?value_converter(apiData.statistics.viewCount):"16K"} &bull; {apiData?moment(apiData.snippet.publishedAt).fromNow():""}</p>
@@ -71,7 +87,7 @@ const PlayVideo = ({videoId}) => {
       <img src={channalData?.snippet?.thumbnails?.default?.url || ""} alt="" />
       <div>
         <p>{apiData ? apiData.snippet.channelTitle : ""}</p>
-        <span>1M Subscribers</span>
+        <span>{channalData?value_converter(channalData.statistics.subscriberCount):"1M"}</span>
       </div>
       <button>Subscribe</button>
        </div>
@@ -80,96 +96,25 @@ const PlayVideo = ({videoId}) => {
         <hr />
         <h4>{apiData?value_converter(apiData.statistics.commentCount):102} comments</h4>
        </div>
-       <div className='comment'>
-        <img src={user_profile} alt="" />
-        <div>
-          <h3>Jach nicholson <span>1 day ago</span></h3>
-          <p>A global computer network provide a varity of imformation and communication faces of interconnected networks using standardized communication</p>
-        <div className="comment-action">
-          <img src={like} alt="" />
-          <span>244</span>
-          <img src={dislike} alt="" />
-        </div>
-        </div>
-       </div>
 
-        <div className='comment'>
-        <img src={user_profile} alt="" />
-        <div>
-          <h3>Jach nicholson <span>1 day ago</span></h3>
-          <p>A global computer network provide a varity of imformation and communication faces of interconnected networks using standardized communication</p>
-        <div className="comment-action">
-          <img src={like} alt="" />
-          <span>244</span>
-          <img src={dislike} alt="" />
-        </div>
-        </div>
-       </div>
+       {commentData.map((item, index) => {
+        return (
+          <div key={index} className='comment'>
+            <img src={item?.snippet?.topLevelComment?.snippet?.authorProfileImageUrl || user_profile} alt="" />
+            <div>
+              <h3>{item?.snippet?.topLevelComment?.snippet?.authorDisplayName || 'Unknown'} <span>1 day ago</span></h3>
+              <p>{item?.snippet?.topLevelComment?.snippet?.textDisplay}</p>
+              <div className="comment-action">
+                <img src={like} alt="" />
+                <span>{value_converter(item?.snippet?.topLevelComment?.snippet?.likeCount || 0)}</span>
+                <img src={dislike} alt="" />
+              </div>
+            </div>
+          </div>
+        )
+       })}
+       
 
-        <div className='comment'>
-        <img src={user_profile} alt="" />
-        <div>
-          <h3>Jach nicholson <span>1 day ago</span></h3>
-          <p>A global computer network provide a varity of imformation and communication faces of interconnected networks using standardized communication</p>
-        <div className="comment-action">
-          <img src={like} alt="" />
-          <span>244</span>
-          <img src={dislike} alt="" />
-        </div>
-        </div>
-       </div>
-
-        <div className='comment'>
-        <img src={user_profile} alt="" />
-        <div>
-          <h3>Jach nicholson <span>1 day ago</span></h3>
-          <p>A global computer network provide a varity of imformation and communication faces of interconnected networks using standardized communication</p>
-        <div className="comment-action">
-          <img src={like} alt="" />
-          <span>244</span>
-          <img src={dislike} alt="" />
-        </div>
-        </div>
-       </div>
-
-        <div className='comment'>
-        <img src={user_profile} alt="" />
-        <div>
-          <h3>Jach nicholson <span>1 day ago</span></h3>
-          <p>A global computer network provide a varity of imformation and communication faces of interconnected networks using standardized communication</p>
-        <div className="comment-action">
-          <img src={like} alt="" />
-          <span>244</span>
-          <img src={dislike} alt="" />
-        </div>
-        </div>
-       </div>
-
-        <div className='comment'>
-        <img src={user_profile} alt="" />
-        <div>
-          <h3>Jach nicholson <span>1 day ago</span></h3>
-          <p>A global computer network provide a varity of imformation and communication faces of interconnected networks using standardized communication</p>
-        <div className="comment-action">
-          <img src={like} alt="" />
-          <span>244</span>
-          <img src={dislike} alt="" />
-        </div>
-        </div>
-       </div>
-
-        <div className='comment'>
-        <img src={user_profile} alt="" />
-        <div>
-          <h3>Jach nicholson <span>1 day ago</span></h3>
-          <p>A global computer network provide a varity of imformation and communication faces of interconnected networks using standardized communication</p>
-        <div className="comment-action">
-          <img src={like} alt="" />
-          <span>244</span>
-          <img src={dislike} alt="" />
-        </div>
-        </div>
-       </div>
 
     </div>  
       
